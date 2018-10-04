92fecfbcc32ec1a2bf6cc1e64f0a8d4321f90a5b
Guillaume Latorre <g.latorre@everlats.com>
2018-10-04 10:56:27 +0200

    change some translations

--- translations/fr.php
+++ translations/fr.php
@@ -13,10 +13,9 @@ $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_07213a0161f52846ab198
 $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_cb5e100e5a9a3e7f6d1fd97512215282'] = 'erreur';
 $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_fd869efa907fb0109eaab503944f1db9'] = 'Ce module vous permet de télécharger les informations de colis de toutes les commandes \"En cours de préparation\" dans un fichier TXT à importer sur ColiShip';
 $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_6c644b9d23ad6e4e467c4094571bef6a'] = 'ColiShip est un logiciel en ligne (remplaçant Expeditor INET) fourni par La Poste pour créer des étiquettes de colis. (Site : https://www.colissimo.fr/entreprise/coliship/)';
-$_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_0f0014bcec39f21bdbfa1b268b718560'] = 'Merci de modifier le jeton ci-dessous pour sécuriser vos données.';
-$_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_3979c5523b4f9fc4cb1637cfccaa03e3'] = 'Merci de renseigner l\'ID du statut des commandes qui seront exportées (exemple : 3, pour les commandes En cours de préparation).';
+$_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_d786f21efbe68e927d31f6126029890d'] = 'Merci de renseigner le statut des commandes qui seront exportées (exemple : les commandes En cours de préparation).';
 $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_f4f70727dc34561dfde1a3c529b6205c'] = 'Réglages';
-$_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_8812b5dba256442fa22e4c7d0343b813'] = 'ID du Statut de commande';
+$_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_96610a96cd3c4694855664bfc1b09098'] = 'Statut des commandes à exporter';
 $_MODULE['<{ev_order2coliship}prestashop>ev_order2coliship_b17f3f4dcf653a5776792498a9b44d6a'] = 'Enregistrer les réglages';
 $_MODULE['<{ev_order2coliship}prestashop>adminorder2colishipcontroller_9ac52b4263427c3c6b26a136dd5c8003'] = 'Commandes vers ColiShip';
 $_MODULE['<{ev_order2coliship}prestashop>adminorder2colishipcontroller_7258e7251413465e0a3eb58094430bde'] = 'Administration';
