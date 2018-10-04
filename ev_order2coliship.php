d66ff492c07d34d26435d71428865759e31fa07f
Guillaume Latorre <g.latorre@everlats.com>
2018-10-04 10:48:30 +0200

    change some texts

--- ev_order2coliship.php
+++ ev_order2coliship.php
@@ -119,8 +119,7 @@ class EV_Order2Coliship extends Module
         		<td style="vertical-align: top;">
         		<b>'.$this->l('This module allows you to download package information into a TXT file for ColiShip.').'</b><br />
         		'.$this->l('ColiShip is a software provided by La Poste to create shipping labels. (website: https://www.colissimo.fr/entreprise/coliship/)').'<br />
-        		<br />'.$this->l('Please change the token below to secure your data.').'<br />
-        		<br />'.$this->l('Please set the order status ID of orders that will be exported (example: 3, which is the processing status).').'<br /></td></tr></table><br />';
+        		<br />'.$this->l('Please set the order status of orders that will be exported (example: processing status).').'<br /></td></tr></table><br />';
     }
 
     public function displayFormSettings()
@@ -134,7 +133,7 @@ class EV_Order2Coliship extends Module
         		<fieldset>
         			<legend><img src="../img/admin/contact.gif" />'.$this->l('Settings').'</legend>
               <br/>
-        			<label>'.$this->l('Order Status ID').'</label>
+        			<label>'.$this->l('Order Status to export').'</label>
         			<div class="margin-form">
         			    <select name="ev_coliship_status_export">';
         			foreach($content as $c) {
